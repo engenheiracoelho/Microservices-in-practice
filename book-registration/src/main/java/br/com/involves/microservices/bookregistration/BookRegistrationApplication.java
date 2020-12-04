249ad6f61bc2af73c9cb07b5aebc6372d79e4f86
leticiacoelho <leticia.ac23@gmail.com>
2020-12-04 14:51:48 -0300

    Add book-registration project

---
+++ book-registration/src/main/java/br/com/involves/microservices/bookregistration/BookRegistrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.involves.microservices.bookregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRegistrationApplication.class, args);
+	}
+
+}
---
+++ book-registration/src/test/java/br/com/involves/microservices/bookregistration/BookRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.involves.microservices.bookregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
