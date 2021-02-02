8e51e40ae03b3e1f9de7c05cba09340daceb7444
Felipe Born de Jesus <felipeborndejesus@hotmail.com>
2021-02-02 11:15:59 -0300

    Meu codigo lixo

--- book-registration-frontend/src/App.js
+++ book-registration-frontend/src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Fragment } from 'react'
+import BookList from './book-list-jesus/BookListJesus';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    
+    <Fragment>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+      <BookList>
+
+      </BookList>
+      
+    </Fragment>
   );
 }
 
