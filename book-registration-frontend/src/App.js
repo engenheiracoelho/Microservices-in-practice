8a70c37677eb9bb21aba5a6fb0d584585745cd99
leticiacoelho <leticia.ac23@gmail.com>
2021-02-02 10:27:23 -0300

    Insert new front-end list

--- book-registration-frontend/src/App.js
+++ book-registration-frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Home from './Test/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GroupList from './Test/BookList';
+import GroupEdit from './Test/BookEdit';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home}/>
+            <Route path='/book' exact={true} component={GroupList}/>
+            <Route path='/book/:id' component={GroupEdit}/>
+          </Switch>
+        </Router>
+    )
+  }
 }
 
 export default App;
